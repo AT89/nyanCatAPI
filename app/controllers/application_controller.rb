789ca2ee5f0843027808fd478713a4e5360da05d
Anh Truong <anh.truong.07@cnu.edu>
2016-08-23 20:36:53 -0400

    verify auth token skip before added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   def cors_set_access_control_headers
+    skip_before_action :verify_authenticity_token
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Allow-Headers'] = '*'
