3dcf0118ff5368091d6cd98a1bba0edb0e6936b5
Dave Corderman <dcordz@gmail.com>
2016-10-14 12:21:38 -0400

    added name validations, not perfect yet

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,25 @@
 class Score < ApplicationRecord
-  validates :name, length: {maximum: 10}
-end
+  before_save { self.name = name.downcase.capitalize }
+
+  VALID_NAME_REGEX = /\A[a-zA-Z]{2,30}($|\s[a-zA-Z]{2,30})\z/i
+
+  validates :name, length: { maximum: 20 }, format: { with: VALID_NAME_REGEX }, uniqueness: { case_sensitive: false }
+
+  validate :allowed_name
+
+private
+
+  def allowed_name
+    self.name = name.downcase.capitalize
+
+    disallowed_words = ["Rat", "John", "Juan"]
+
+    disallowed_words.each do |word|
+      if self.name == word
+        errors.add(:base, "Please choose a different name.")
+        return false
+      end #end if
+    end #end enumerator
+  end #end method
+  
+end #end class
