f062cfa7851fe01f0f651678bb5eb66a1ca9916a
Anh <anh.truong.07@cnu.edu>
2016-10-19 19:25:07 -0400

    fixed typo on application controller causing api to fail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*http://anhtice.com/nyancat
+    headers['Access-Control-Allow-Origin'] = 'http://anhtice.com/nyancat'
     headers['Access-Control-Allow-Methods'] = 'POST, GET'
     headers['Access-Control-Allow-Headers'] = '*'
     headers['Access-Control-Max-Age'] = "1728000"
